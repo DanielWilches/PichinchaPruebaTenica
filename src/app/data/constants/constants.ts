c06205318bb81e66bfb9df27164d35b477e08842
DanielWilches <danielprietowilche@gmail.com>
2024-12-11 20:59:54 -0500

    Creacion De constatnes

---
+++ src/app/data/constants/constants.ts
@@ -0,0 +1,10 @@
+export  const URL_BASE ='https://tribu-ti-staffing-desarrollo-afangwbmcrhucqfh.z01.azurefd.net';
+export  const IPF_MSA ='/ipf-msa-productosfinancieros';
+export  const BP_PRODUCTS ='/bp/products';
+export  const BP_PRODUCTS_VERIFICATION ='/bp/products/verification';
+
+export const CODE_200 ="200";
+export const CODE_206 ="206";
+export const CODE_400 ="400";
+export const CODE_401 ="401";
+export const CODE_404 ="404";
